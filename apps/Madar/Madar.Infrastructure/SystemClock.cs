198b1a918eb75c155204fb10cd13fbe5c067f761
ALHassan ALShami <hassan1alshami6@gmail.com>
2026-08-10 18:41:09 +0300

    Remove Athar and Madar product trees from Core-focused repository

--- apps/Madar/Madar.Infrastructure/SystemClock.cs
+++
@@ -1,8 +0,0 @@
-using FoundationKit.Application.Abstractions;
-
-namespace Madar.Infrastructure;
-
-public sealed class SystemClock : IClock
-{
-    public DateTimeOffset UtcNow => DateTimeOffset.UtcNow;
-}
--- examples/Athar/Athar.Infrastructure/SystemClock.cs
+++
@@ -1,8 +0,0 @@
-using FoundationKit.Application.Abstractions;
-
-namespace Athar.Infrastructure;
-
-public sealed class SystemClock : IClock
-{
-    public DateTimeOffset UtcNow => DateTimeOffset.UtcNow;
-}
