cbbfcef9627c94f44acddeca6534e0ae41176564
ALHassan ALShami <hassan1alshami6@gmail.com>
2026-08-17 11:16:46 +0300

    Use native TimeProvider for Workbench clock

--- samples/FoundationKit.Workbench/Infrastructure/SystemClock.cs
+++ samples/FoundationKit.Workbench/Infrastructure/SystemClock.cs
@@ -2,7 +2,10 @@ using FoundationKit.Application.Abstractions;
 
 namespace FoundationKit.Workbench.Infrastructure;
 
-public sealed class SystemClock : IClock
+public sealed class SystemClock(TimeProvider timeProvider) : IClock
 {
-    public DateTimeOffset UtcNow => DateTimeOffset.UtcNow;
+    private readonly TimeProvider _timeProvider = timeProvider
+        ?? throw new ArgumentNullException(nameof(timeProvider));
+
+    public DateTimeOffset UtcNow => _timeProvider.GetUtcNow();
 }
